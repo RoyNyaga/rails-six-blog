8acfb79ffba22fd51e4408f807218153b912f5f8
RoyNyaga <nyagaandreroy@gmail.com>
2020-09-15 17:24:30 +0100

    migrate element model

--- db/migrate/20200914154643_create_elements.rb
+++ db/migrate/20200914154643_create_elements.rb
@@ -2,7 +2,7 @@ class CreateElements < ActiveRecord::Migration[6.0]
   def change
     create_table :elements do |t|
       t.string :element_type
-      t.text, :content
+      t.text :content
       t.references :post, null: false, foreign_key: true
       t.integer :position
 
